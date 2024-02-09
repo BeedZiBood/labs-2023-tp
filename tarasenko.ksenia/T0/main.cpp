bf691c53311234c3362769b371d2941504fbbe08
BeedZiBood <117881296+BeedZiBood@users.noreply.github.com>
2024-02-09 20:37:24 +0300

    Delete tarasenko.ksenia directory

--- tarasenko.ksenia/T0/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-  std::cout << "tarasenko.ksenia\n";
-  return 0;
-}
--- tarasenko.ksenia/common/read_trash.cpp
+++
@@ -1,9 +0,0 @@
-#include "read_trash.h"
-
-std::istream& tarasenko::readTrash(std::istream& input)
-{
-  input.clear();
-  std::string trash = " ";
-  getline(input, trash);
-  return input;
-}
